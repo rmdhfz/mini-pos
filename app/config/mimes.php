87afcc0dd4f17287680072f43f3d898fc1c6adc0
Hafiz Ramadhan <hfzrmd@gmail.com>
2022-05-31 22:16:39 +0700

    add: webp in app/config/mimes.php

--- app/config/mimes.php
+++ app/config/mimes.php
@@ -77,6 +77,7 @@ return array(
 	'mj2'	=>	array('image/jp2', 'video/mj2', 'image/jpx', 'image/jpm'),
 	'mjp2'	=>	array('image/jp2', 'video/mj2', 'image/jpx', 'image/jpm'),
 	'png'	=>	array('image/png',  'image/x-png'),
+	'webp'	=>	array('image/webp'),
 	'tiff'	=>	'image/tiff',
 	'tif'	=>	'image/tiff',
 	'css'	=>	array('text/css', 'text/plain'),
